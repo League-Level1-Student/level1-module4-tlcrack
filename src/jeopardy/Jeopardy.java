683613c7759adb36ec5a60ca281218020de150c0
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-03-31 12:45:29 -0700

    simonSays, Jeopardy(ip)

--- src/jeopardy/Jeopardy.java
+++ src/jeopardy/Jeopardy.java
@@ -50,7 +50,7 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+frame.setVisible(true);
 		// 2. Give your frame a title
 
 		// 3. Create a JPanel variable to hold the header using the createHeader method
